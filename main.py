9e065bda430396f2a81b924fe50c8d0ee6536bfd
ramboo-raajesh <100186781+ramboo-raajesh@users.noreply.github.com>
2022-07-09 13:40:50 +0530

    Update main.py
    . Mail deliveried as per ID's given by the user
    . Fixed less app security

--- main.py
+++ main.py
@@ -2,34 +2,18 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 
-subject = "Email From Python"
-body = "A message"
-sender_email = "greatraajesh10@gmail.com"
-receiver_email = "greatraajesh10@gmail.com"
-password = "9566874940aa"
-
+subject = input("Enter Your Subject: ")
+body = input("Enter Your Body of the Mail ")
+sender_email = input("Enter Your email address ")
+receiver_email = input("Enter Receiver email adress ")
+password = input("Enter Your email adress password ")
 message = EmailMessage()
 message["From"] = sender_email
 message["To"] = receiver_email
 message["Subject"] = subject
-
-html = f"""
-<html>
-    <body>
-        <h1>{subject}</h1>
-        <p>{body}</p>
-    </body>
-</html>
-"""
-
-message.add_alternative(html, subtype="html")
-
 context = ssl.create_default_context()
-
 print("Sending Email!")
-
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message.as_string())
-
 print("Success")
